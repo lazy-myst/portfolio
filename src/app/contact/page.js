46a4594d545a477df497340aaa0e6d7849459b75
Muhammad Ammad <139933656+lazy-myst@users.noreply.github.com>
2024-09-16 04:54:54 +0500

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -50,69 +50,7 @@ const page = () => {
                             </Link>
                         </div>
                     </m.div>
-                </div>
-                <div className="flex hover:opacity-70">
-                    <m.div
-                        initial={{ filter: 'blur(10px)' }}
-                        whileInView={{ filter: 'blur(0px)' }}
-                        viewport={{ once: true, margin: '0px 0px -200px 0px' }}
-                        transition={{ duration: .5, delay: .5 }}
-                        className=" flex  items-center justify-center">
-                        <Image
-                            src="/iglogoCon.png"
-                            height={100}
-                            width={100}
-                            className="p-5"
-                        />
-                        <div className="flex text-xl ml-3 items-center justify-center">
-                            <Link
-                                target="_blank"
-                                href='https://www.instagram.com/aint_myst/'>aint_myst
-                            </Link>
-                        </div>
-                    </m.div>
-                </div>
-                <div className="flex hover:opacity-70">
-                    <m.div
-                        initial={{ filter: 'blur(10px)' }}
-                        whileInView={{ filter: 'blur(0px)' }}
-                        viewport={{ once: true, margin: '0px 0px -200px 0px' }}
-                        transition={{ duration: .5, delay: .5 }}
-                        className=" flex items-center justify-center">
-                        <Image
-                            src="/fblogoCon.png"
-                            height={100}
-                            width={100}
-                            className="p-4"
-                        />
-                        <div className="flex ml-3 text-xl items-center justify-center">
-                            <Link
-                                target="_blank"
-                                href='https://www.facebook.com/aint.myst/'>Aint Myst
-                            </Link>
-                        </div>
-                    </m.div>
-                </div>
-                <div className="flex hover:opacity-70">
-                    <m.div
-                        initial={{ filter: 'blur(10px)' }}
-                        whileInView={{ filter: 'blur(0px)' }}
-                        viewport={{ once: true, margin: '0px 0px -200px 0px' }}
-                        transition={{ duration: .5, delay: .5 }}
-                        className=" flex  items-center justify-center">
-                        <Image
-                            src="/tNewlogoCon.png"
-                            height={100}
-                            width={100}
-                            className="p-6"
-                        />
-                        <div className="flex ml-3 text-xl items-center justify-center">
-                            <Link
-                                target="_blank"
-                                href='https://twitter.com/MYSTGAMING5'>Ain't Myst
-                            </Link>
-                        </div>
-                    </m.div>
+                </div>v>
                 </div>
             </div>
         </main>
